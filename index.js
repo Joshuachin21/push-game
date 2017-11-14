d9997b8a3c24229ec44c6fe8ff8bfc642e10708a
Mark Zipagang <mzipagang@gmail.com>
2017-11-14 14:48:07 -0800

    added counts

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 var Gpio = require('onoff').Gpio,
     led = new Gpio(17, 'out'),
     button = new Gpio(4, 'in', 'both');
-
+let count = 0;
 button.watch(function (err, value) {
     if (err) {
         throw err;
     }
 console.log('button pushed');
-    console.log(value);
-    led.writeSync(value);
+
+    if(value === 0){
+        count += 1;
+    }
+    console.log(count);
+    //led.writeSync(value);
 });
 
 process.on('SIGINT', function () {
