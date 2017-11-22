5a7d2164dc3955ea4e6a738cf0bdf3faa3236cac
Mark Zipagang <mzipagang@gmail.com>
2017-11-22 00:16:08 -0800

    bug fixes

--- index.js
+++ index.js
@@ -50,6 +50,9 @@ setInterval(function(){
     console.log('Player 2 scores:' + count2);
 }, 2000);*/
 
+
+var gameSeconds = 0;
+
 setInterval(function(){
 
     if(count1>count2){
@@ -64,6 +67,19 @@ setInterval(function(){
 
     console.log('====================================================');
 
-    console.log('Player 1 -------------------------- ' + count1 + ' clicks!');
-    console.log('Player 2 -------------------------- ' + count2 + ' clicks!');
+    console.log('Player 1 -------------------------- ' + count1 + ' clicks at ' + (count1 / gameSeconds) + 'Clicks per Second!');
+    console.log('Player 2 -------------------------- ' + count2 + ' clicks at ' + (count2 / gameSeconds) + 'Clicks per Second!');
 }, 10000);
+
+
+var pps1 = 0;
+var pps2 = 0;
+setInterval(function(){
+ gameSeconds = gameSeconds+1;
+    console.log('==========================PPS==========================');
+
+    console.log('Player 1 -------------------------- ' + pps1 + ' PPS!');
+    console.log('Player 2 -------------------------- ' + pps2 + ' PPS!');
+    pps1 = 0;
+    pps2 = 0;
+}, 1000);
