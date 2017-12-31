1f58f77de63a0a757c6047a62024cb15879eb14c
Mark Zipagang <mzipagang@gmail.com>
2017-12-31 00:12:20 -0800

    no message

--- public/sound.js
+++ public/sound.js
@@ -1,19 +1,12 @@
 var Sound = require('node-aplay');
  
 // fire and forget: 
-new Sound('/home/pi/Music/sf3sound/20H.wav').play();
+new Sound('/home/pi/Music/sf3sound/2AH.wav').play();
  
 // with ability to pause/resume: 
-var music = new Sound('/path/to/the/file/filename.wav');
+var music = new Sound('/home/pi/Music/sf3sound/20H.wav');
 music.play();
  
-setTimeout(function () {
-    music.pause(); // pause the music after five seconds 
-}, 5000);
- 
-setTimeout(function () {
-    music.resume(); // and resume it two seconds after pausing 
-}, 7000);
  
 // you can also listen for various callbacks: 
 music.on('complete', function () {
