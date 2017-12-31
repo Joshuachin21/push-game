5f272a7fc42cbd96e3ff5ebeb1d944b14dcb5b7f
Mark Zipagang <mzipagang@gmail.com>
2017-12-30 23:03:38 -0800

    new sound lib

--- public/sound.js
+++ public/sound.js
@@ -1,19 +1,21 @@
-var soundplayer = require("sound-player");
-var options = {
-    filename: "/home/pi/Music/sf3sound/20H.wav",
-    gain: 100,
-    debug: true,
-    player: "aplay", // "afplay" "aplay" "mpg123" "mpg321"
-    device: "plughw:0,0"   //
-}
-
-var player = new soundplayer(options)
-player.play();
-
-player.on('complete', function() {
+var Sound = require('node-aplay');
+ 
+// fire and forget: 
+new Sound('/path/to/the/file/filename.wav').play();
+ 
+// with ability to pause/resume: 
+var music = new Sound('/path/to/the/file/filename.wav');
+music.play();
+ 
+setTimeout(function () {
+    music.pause(); // pause the music after five seconds 
+}, 5000);
+ 
+setTimeout(function () {
+    music.resume(); // and resume it two seconds after pausing 
+}, 7000);
+ 
+// you can also listen for various callbacks: 
+music.on('complete' function () {
     console.log('Done with playback!');
-});
-
-player.on('error', function(err) {
-    console.log('Error occurred:', err);
 });
